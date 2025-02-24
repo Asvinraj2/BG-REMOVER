eccbf524224ea084ffc9af8e00990f726d94a62b
Asvinraj2 <aswinraj2002.as@gmail.com>
2025-02-24 12:01:55 +0530

    removed cmnts from server.js

--- server/server.js
+++ server/server.js
@@ -25,6 +25,12 @@ app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use(cors())
 
+app.use(cors({
+    origin: 'https://bg-remover-zeta.vercel.app', // Allow only your deployed frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+    credentials: true // If you're using cookies for authentication
+  }))
+
 // Serve uploads directory
 app.use('/uploads', express.static(uploadsDir))
 
@@ -50,29 +56,3 @@ try {
     console.error("Failed to start server:", error)
     process.exit(1)
 }
-
-
-
-
-// import 'dotenv/config'
-// import express from 'express'
-// import cors from 'cors'
-// import connectDB from './configs/mongodb.js'
-// import userRouter from './routes/userRoutes.js'
-
-
-// // App Config
-
-// const PORT = process.env.PORT || 4000
-// const app = express()
-// await connectDB()
-
-// //initialize middlewares
-// app.use(express.json())
-// app.use(cors())
-
-// //API route
-// app.get('/', (req,res) => res.send("API Working"))
-// app.use('/api/user', userRouter)
-
-// app.listen(PORT, () => console.log("Server Running on PORT "+PORT))
